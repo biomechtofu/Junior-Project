c00448694bb2808a4eb6c6796403bc70c635ab28
biomechtofu <andrewkwokninja@gmail.com>
2018-10-20 23:50:46 -0700

    UART Test

---
+++ Objects/uart test.d
@@ -0,0 +1,4 @@
+.\objects\uart test.o: UART Test.c
+.\objects\uart test.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\objects\uart test.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
+.\objects\uart test.o: C:\ti\TivaWare_C_Series-2.1.4.178\inc\tm4c123gh6pm.h
---
+++ UART Test.c
@@ -0,0 +1,11 @@
+#include <stdint.h>
+#include <stdbool.h>
+#include "tm4c123gh6pm.h"
+
+void UARTIntHandler(void)
+{
+	uint32_t ui32Status;
+	
+	
+	
+}
